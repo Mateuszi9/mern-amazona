c97e601400b1a646d9ef4718d9c7b3a0e3471ab2
Mateusz Izdebski <mati2k51@gmail.com>
2022-11-04 18:57:45 +0100

    Lesson-14-Create-React-Context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StroeProvider } from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <StroeProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StroeProvider>
   </React.StrictMode>
 );
 
