4e4f6e641c73261a9da26cb52c5c342e68a0ccb5
Mateusz Izdebski <mati2k51@gmail.com>
2022-11-04 21:10:30 +0100

    Lesson-15-Improve-Add-To-Cart

--- backend/data.js
+++ backend/data.js
@@ -1,30 +1,33 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/p1.jpg', // 679 x 829
       price: 120,
-      countInStock: 10,
+      countInStock: 2,
       brand: 'Nike',
       rating: 4.5,
       numReviews: 10,
       decription: 'high quality shirt',
     },
     {
+      _id: '2',
       name: 'Adidas Fit shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
       image: '/images/p2.jpg',
       price: 250,
-      countInStock: 20,
+      countInStock: 0,
       brand: 'Adidas',
       rating: 4.0,
       numReviews: 10,
       decription: 'high quality shirt',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -37,6 +40,7 @@ const data = {
       decription: 'high quality shirt',
     },
     {
+      _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
